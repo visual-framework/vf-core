2f1da04920f3ce9bb1a9245600ea8800c8519ee1
Ken Hawkins <khawkins98@gmail.com>
2018-11-12 10:17:20 +0100

    Update vf-data-protection-banner rough edges (#20)
    For #16 
    
    #changelog #patterns

--- components/blocks/vf-data-protection-banner/vf-data-protection-banner.js
+++ components/blocks/vf-data-protection-banner/vf-data-protection-banner.js
@@ -1,12 +1,12 @@
 // vf-data-protecion-banner
 
 var remove = function(){
-    this.parentNode.remove();
+  this.closest('.vf-data-protection-banner').remove();
 };
 
 var GDPRBanner = document.querySelectorAll('.vf-data-protection-banner');
 var GDPRButton = document.querySelectorAll('.vf-data-protection-banner__button');
 
 for (var i = 0, len = GDPRBanner.length; i < len; i++) {
-    GDPRButton[i].addEventListener('click', remove, false);
+  GDPRButton[i].addEventListener('click', remove, false);
 }
