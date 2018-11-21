19cf7225e02e5a0cd4b672ddb7b1d1592fb22342
Ken Hawkins <khawkins98@gmail.com>
2018-11-21 14:51:30 +0100

    patterns/embl-meta-tags
    Adds an inital spec for the meta tags. These can be queries by other patterns (such as the breadcrumb or global nav) to provide appropriate navigation and loook/feel.

---
+++ components/blocks/embl-content-meta-properties/embl-content-meta-properties.js
@@ -0,0 +1,4 @@
+// embl-content-meta-properties
+
+// To come, but in addition to being queried by other patterns' JS, this could
+// also add classes to a page to affect the overall look of a page.
