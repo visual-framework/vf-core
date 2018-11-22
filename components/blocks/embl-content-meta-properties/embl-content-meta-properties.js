77b2d8d210fddec038b1a4721871be318c4843d2
Ken Hawkins <khawkins98@gmail.com>
2018-11-22 13:03:12 +0100

    Add proof of concept for metatag reading

--- components/blocks/embl-content-meta-properties/embl-content-meta-properties.js
+++ components/blocks/embl-content-meta-properties/embl-content-meta-properties.js
@@ -1,4 +1,62 @@
 // embl-content-meta-properties
 
-// To come, but in addition to being queried by other patterns' JS, this could
+// In addition to being queried by other patterns' JS, this could
 // also add classes to a page to affect the overall look of a page.
+
+
+/**
+ * Read metaProperties from existing metaTags and then runs
+ * emblContentMetaProperties_Process()
+ * @example emblContentMetaProperties_Read()
+ */
+function emblContentMetaProperties_Read() {
+  var metaProperties = {};
+  // <!-- Content descriptors -->
+  // <meta name="embl:who" content="{{ meta-who }}"> <!-- the people, groups and teams involved -->
+  // <meta name="embl:where" content="{{ meta-where }}"> <!-- at which EMBL sites the content applies -->
+  // <meta name="embl:what" content="{{ meta-what }}"> <!-- the activities covered -->
+  metaProperties.who = metaProperties.who || document.querySelector("meta[name='embl:who']");
+  metaProperties.what = metaProperties.what || document.querySelector("meta[name='embl:what']");
+  metaProperties.who = metaProperties.who || document.querySelector("meta[name='embl:who']");
+
+  // <!-- Content role -->
+  // <meta name="embl:utility" content="-8"> <!-- if content is task and work based or if is meant to inspire -->
+  // <meta name="embl:reach" content="-5"> <!-- if content is externally (public) or internally focused (those that work at EMBL) -->
+  metaProperties.utility = metaProperties.utility || document.querySelector("meta[name='embl:utility']");
+  metaProperties.reach = metaProperties.reach || document.querySelector("meta[name='embl:reach']");
+
+  // <!-- Page infromation -->
+  // <meta name="embl:maintainer" content="{{ meta-maintainer }}"> <!-- the contact person or group responsible for the page -->
+  // <meta name="embl:last-review" content="{{ meta-last-review }}"> <!-- the last time the page was reviewed or updated -->
+  // <meta name="embl:review-cycle" content="{{ meta-review-cycle }}"> <!-- how long in days before the page should be checked -->
+  // <meta name="embl:expiry" content="{{ meta-expiry }}"> <!-- if there is a fixed point in time when the page is no longer relevant -->
+  metaProperties.maintainer = metaProperties.maintainer || document.querySelector("meta[name='embl:maintainer']");
+  metaProperties.lastReview = metaProperties.lastReview || document.querySelector("meta[name='embl:last-review']");
+  metaProperties.reviewCycle = metaProperties.reviewCycle || document.querySelector("meta[name='embl:review-cycle']");
+  metaProperties.expiry = metaProperties.expiry || document.querySelector("meta[name='embl:expiry']");
+
+  for(var key in metaProperties) {
+    if (metaProperties[key] != null && metaProperties[key].getAttribute("content").length != 0) {
+      metaProperties[key] = metaProperties[key].getAttribute("content");
+    } else {
+      metaProperties[key] = 'notSet';
+    }
+  }
+
+  emblContentMetaProperties_Process(metaProperties);
+}
+
+/**
+ * Take any appropriate actions depending on present metaTags
+ * @example emblContentMetaProperties_Process()
+ * @param {object} [metaProperties] - if you do not have meta tags on the page,
+ *                                    you can explicitly pass options
+ */
+function emblContentMetaProperties_Process(metaProperties) {
+  var metaProperties = metaProperties || {a: a, b: b, c: c};
+
+  console.log('readTags',metaProperties)
+}
+
+// Process metaProperties
+emblContentMetaProperties_Read();
