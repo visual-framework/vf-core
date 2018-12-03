4e2811b83b0961d5e664a6ff3c8ddbf4a1bce71a
Stuart Robson <stu@alwaystwisted.com>
2018-12-03 14:49:44 +0000

    adds style lint to gulp tasks (#73)
    * adds stylelint dependency and adds it to gulp dev task
    
    * all the stylelint fixes

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,14 @@ const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 const nodeModuleImport = require('@node-sass/node-module-importer');
-const sassLint = require('gulp-sass-lint');
 const recursive = require('./tools/css-generator/recursive-readdir');
+
+// Linting things
+const prettier = require('gulp-prettier');
+const postcss     = require('gulp-postcss');
+const reporter    = require('postcss-reporter');
+const syntax_scss = require('postcss-scss');
+const stylelint   = require('stylelint');
 
 // JS Stuff
 const concat = require('gulp-concat');
@@ -85,27 +91,83 @@ gulp.task('css', function() {
 });
 
 // Sass Lint
-gulp.task('linting', function(done) {
-  gulp
-    .src(['./components/**/*.s+(a|c)ss', './assets/scss/***.s+(a|c)ss'])
-    .pipe(
-      sassLint({
-        files: {},
-        rules: {
-          'class-name-format': 0,
-          'variable-name-format': 0,
-          'no-empty-rulesets': 0,
-          'final-newline': 0,
-          'space-around-operator': 0,
-          'space-after-comma': 0,
-          'mixin-name-format': 0
+gulp.task("scss-lint", function() {
+
+  // Stylelint config rules
+  var stylelintConfig = {
+    "plugins": [
+      "stylelint-order",
+      "stylelint-scss"
+    ],
+    "extends": [
+      "./node_modules/prettier-stylelint/config.js"
+    ],
+    "rules": {
+      "indentation": 2,
+      "string-quotes": "single",
+      "order/order": [
+        [
+        "custom-properties",
+        "dollar-variables",
+        {
+        type: "at-rule",
+        name: "include",
+        parameter: "set-*",
+        message: "Stop being lazy and flatten your classnames"
+        },
+        "declarations",
+        "rules",
+        {
+        	type: "at-rule",
+        	name: "media",
+        	hasBlock: true
         }
-      })
+      ]
+      ],
+      "order/properties-alphabetical-order": true,
+      "block-no-empty": true,
+      "color-no-invalid-hex": true,
+      "declaration-colon-space-after": "always",
+      "declaration-colon-space-before": "never",
+      "function-comma-space-after": "always",
+      "media-feature-colon-space-after": "always",
+      "media-feature-colon-space-before": "never",
+      "media-feature-name-no-vendor-prefix": true,
+      "max-empty-lines": 3,
+      "max-nesting-depth": 3,
+      "selector-nested-pattern": [ "^(?!&__|&--|&-|&_).*", {
+        "message": "Stop being lazy and flatten your classnames",
+        "severity": "warning"
+      } ],
+      "number-leading-zero": "never",
+      "number-no-trailing-zeros": true,
+      "property-no-vendor-prefix": true,
+      "selector-list-comma-space-before": "never",
+      "selector-list-comma-newline-after": "always",
+      "string-quotes": "single",
+      "value-no-vendor-prefix": true,
+      // "indentation": [ 2, {
+      //   "except": ["block"],
+      //   "message": "Please use 2 spaces for indentation. Tabs make Stuart very grumpy.",
+      //   "severity": "warning"
+      // } ]
+    }
+  }
+
+  var processors = [
+    stylelint(stylelintConfig),
+    reporter({
+      clearMessages: true,
+      throwError: false,
+    })
+  ];
+
+  return gulp.src(
+      ['components/**/vf-*.scss', '!components/**/index.scss', '!assets/**/*.scss']
     )
-    .pipe(sassLint.format())
-    .pipe(sassLint.failOnError());
-  done();
+    .pipe(postcss(processors, {syntax: syntax_scss}));
 });
+
 
 // -----------------------------------------------------------------------------
 // Scripts Tasks
@@ -258,8 +320,9 @@ gulp.task('CSSGen', function(done) {
 
 gulp.task('watch', function(done) {
   fractal.watch();
-  gulp.watch('./**/*.scss', gulp.series('css')).on('change', reload);
-  gulp.watch(['./components/**/*.js'], gulp.series('scripts')).on('change', reload);
+  gulp.watch('./**/*.scss', gulp.series(['css', 'scss-lint'])).on('change', reload);
+  gulp.watch('./assets/images', gulp.series('watch-images')).on('change', reload);
+  gulp.watch(['./assets/scripts/**/*.js','./components/**/*.js'], gulp.series('scripts')).on('change', reload);
   gulp.watch('./components/**/**/assets/*', gulp.series('pattern-assets')).on('change', reload);
 });
 
@@ -270,7 +333,7 @@ gulp.task('watch', function(done) {
 
 // Build as a static site for CI
 gulp.task('build', gulp.series(
-    'css', 'pattern-assets', 'scripts', 'frctlBuild'
+    'scss-lint', 'css', 'pattern-assets', 'scripts', 'frctlBuild'
 ));
 
 gulp.task('dev', gulp.parallel(
