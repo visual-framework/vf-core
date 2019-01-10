60dc21f79ab24ad9171f7e8d6a8723dab1e3206f
Ken Hawkins <khawkins98@gmail.com>
2019-01-10 19:33:50 +0100

    Conditionally enable edit links through JS

--- components/embl-conditional-edit/embl-conditional-edit.js
+++ components/embl-conditional-edit/embl-conditional-edit.js
@@ -1,25 +1,50 @@
 // embl-conditional-edit
+
 /**
- * Finds all trello lists on a page and activates them
- * @example emblGetTrelloLists()
+ * Invoke emblConditionalEditDetectParam scopped to objects where
+ * data-embl-js-conditional-edit is present
  */
 function emblConditionalEdit() {
-  // const emblTrelloLists = document.querySelectorAll('[data-embl-js-trellolist]');
-  // if (!emblTrelloLists) {
-  //   // exit: lists not found
-  //   return;
-  // }
-  // if (emblTrelloLists.length == 0) {
-  //   // exit: lists not found
-  //   return;
-  // }
-  //
-  //
-  //
-  // Array.prototype.forEach.call(emblTrelloLists, (list, i) => {
-  //   // getTrelloList('5c3362c941fd3e51e861750a','not-started','Not started');
-  //   getTrelloList(list.dataset.emblJsTrellolistId,list.dataset.emblJsTrellolistName);
-  // });
+  const emblConditionalEdit = document.querySelectorAll('[data-embl-js-conditional-edit]');
+  if (!emblConditionalEdit) {
+    // exit: lists not found
+    return;
+  }
+  if (emblConditionalEdit.length == 0) {
+    // exit: lists not found
+    return;
+  }
+
+  Array.prototype.forEach.call(emblConditionalEdit, (element, i) => {
+    emblConditionalEditDetectParam(location.href,element);
+  });
+
+
+
+}
+/**
+ * Detects `?embl-conditional-edit=enabled` or `?embl-conditional-edit=1`
+ * and adds `.embl-coditional-edit__enabled` to display the edit links
+ * @param {string} [url] - the url to check for an enabling param
+ * @param {element} [element] - the scopped element to be processed
+ */
+function emblConditionalEditDetectParam(url,element) {
+  var captured = /embl-conditional-edit=([^&]+)/.exec(url);
+  if (captured == null) {
+    // value not found
+
+    // also try against any parent iframe url
+    if (window.self !== window.top) {
+      emblConditionalEditDetectParam(parent.window.location,element);
+    }
+
+    return;
+  }
+  captured = captured[1];
+
+  if (captured == '1' || captured == 'enabled') {
+    element.className += ' ' + 'embl-coditional-edit__enabled';
+  }
 }
 
 emblConditionalEdit();
