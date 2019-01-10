26ae5173330295d7e1d02cf052428d7f2bc9cbc8
Ken Hawkins <khawkins98@gmail.com>
2019-01-10 15:00:38 +0100

    JS Scaffolding

--- components/embl-conditional-edit/embl-conditional-edit.js
+++ components/embl-conditional-edit/embl-conditional-edit.js
@@ -1,1 +1,25 @@
 // embl-conditional-edit
+/**
+ * Finds all trello lists on a page and activates them
+ * @example emblGetTrelloLists()
+ */
+function emblConditionalEdit() {
+  // const emblTrelloLists = document.querySelectorAll('[data-embl-js-trellolist]');
+  // if (!emblTrelloLists) {
+  //   // exit: lists not found
+  //   return;
+  // }
+  // if (emblTrelloLists.length == 0) {
+  //   // exit: lists not found
+  //   return;
+  // }
+  //
+  //
+  //
+  // Array.prototype.forEach.call(emblTrelloLists, (list, i) => {
+  //   // getTrelloList('5c3362c941fd3e51e861750a','not-started','Not started');
+  //   getTrelloList(list.dataset.emblJsTrellolistId,list.dataset.emblJsTrellolistName);
+  // });
+}
+
+emblConditionalEdit();
