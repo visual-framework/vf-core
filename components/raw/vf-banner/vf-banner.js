af3a47ee44383f738442e3a311304fa40cd7768f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-22 15:09:20 +0000

    Deploy visual-framework/vf-core to github.com/visual-framework/vf-core.git:gh-pages

--- components/raw/vf-banner/vf-banner.js
+++ components/raw/vf-banner/vf-banner.js
@@ -1,12 +1,223 @@
 // vf-banner
 
-var remove = function(){
-  this.closest('.vf-banner--gdpr').remove();
-};
 
-var GDPRBanner = document.querySelectorAll('.vf-banner--gdpr');
-var GDPRButton = document.querySelectorAll('.vf-banner--gdpr .vf-button');
+// Turn the below code snippet into a banner
+// <div class="vf-banner vf-banner--fixed vf-banner--bottom vf-banner--notice"
+// data-vf-js-banner
+// data-vf-js-banner-state="persistent|dismissible|blocking" data-vf-js-banner-esc-close="y|n"
+// data-vf-js-banner-cookie-name="{{data-service-id}}"
+// data-vf-js-banner-cookie-version="{{data-protection-version}}"
+// data-vf-js-banner-extra-button="<a href='#'>string1</a><a href='#'>string2</a>">
+//   <div class="vf-banner__content | vf-grid">
+//     <p class="vf-text vf-text--body-l">
+//       This website uses cookies, and the limiting processing of your personal data to function. By using the site you are agreeing to this as outlined in our <a class="vf-link" href="JavaScript:Void(0);">Privacy Notice</a> and <a class="vf-link" href="JavaScript:Void(0);">Terms Of Use</a>.
+//     </p>
+//
+//     <button class="vf-button vf-button--secondary">
+//       {{vf-data-protection-banner__link}}
+//     </button>
+//   </div>
+  // </div>
 
-for (var i = 0, len = GDPRBanner.length; i < len; i++) {
-  GDPRButton[i].addEventListener('click', remove, false);
+/**
+ * Clear the cooke. This is mostly a development tool.
+ */
+function vfBannerReset(vfBannerCookieNameAndVersion) {
+  vfBannerSetCookie(vfBannerCookieNameAndVersion,false);
 }
+
+/**
+ * Confirm a banner, initiate cookie logging
+ */
+function vfBannerConfirm(banner,vfBannerCookieNameAndVersion) {
+  banner.classList += " vf-u-display-none";
+  if (vfBannerCookieNameAndVersion !== 'null') {
+    vfBannerSetCookie(vfBannerCookieNameAndVersion,true);
+  }
+}
+
+/**
+ * Log a cookie
+ */
+function vfBannerSetCookie(c_name, value, exdays) {
+  // var value = value || 'true';
+  var exdays = exdays || 90;
+  var exdate = new Date();
+  var c_value;
+  exdate.setDate(exdate.getDate() + exdays);
+  c_value = escape(value) + (exdays === null ? "" : ";expires=" + exdate.toUTCString()) + ";domain=" + document.domain + ";path=/";
+  document.cookie = c_name + "=" + c_value;
+}
+
+/**
+ * See if a cookie has been set
+ */
+function vfBannerGetCookie(c_name) {
+  var i,
+      x,
+      y,
+      ARRcookies = document.cookie.split(";");
+  for (i = 0; i < ARRcookies.length; i++) {
+    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+    y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+    x = x.replace(/^\s+|\s+$/g, "");
+    if (x === c_name) {
+      return unescape(y);
+    }
+  }
+}
+
+
+
+/**
+ * Finds all vf-banner on a page and activates them
+ * @example vfBanner()
+ */
+function vfBanner() {
+  const bannerList = document.querySelectorAll('[data-vf-js-banner]');
+
+  if (!bannerList) {
+    // exit: banners not found
+    return;
+  }
+  if (bannerList.length == 0) {
+    // exit: banner content not found
+    return;
+  }
+
+  // generate the banner pattern, js events
+  Array.prototype.forEach.call(bannerList, (banner, i) => {
+
+    // map the JS data attributes to our object structure
+    var bannerRemapped = banner.dataset;
+    // pickup text from existing element
+    bannerRemapped.vfJsBannerText = banner.querySelectorAll('[data-vf-js-banner-text]')[0].innerHTML;
+
+    var uniqueId = Math.round(Math.random()*10000000);
+
+    // set an id to target this banner
+    banner.setAttribute('data-vf-js-banner-id',uniqueId);
+
+    // preserve the classlist
+    bannerRemapped.classList = banner.querySelectorAll('[data-vf-js-banner-text]')[0].classList;
+
+    // Make the banner come alive
+    vfBannerInsert(bannerRemapped,uniqueId);
+  });
+
+}
+
+/**
+ * Takes a banner object and creates the necesary html markup, js events, and inserts
+ * @example vfBannerInsert()
+ * @param {object} [banner]  -
+ * @param {string} [bannerId] - the id of the target div, `data-vf-js-banner-id="1"`
+ */
+function vfBannerInsert(banner,bannerId) {
+  var targetBanner = document.querySelectorAll('[data-vf-js-banner-id="'+bannerId+'"]')[0];
+  if (targetBanner == undefined) {
+    return;
+  }
+  console.log(banner)
+  var generatedBannerHtml = '<div class="'+banner.classList+'">';
+
+  generatedBannerHtml += banner.vfJsBannerText;
+
+  // What type of banner?
+  if (banner.vfJsBannerState === 'persistent') {
+    // nothing more to do for persistent, you can't close it
+
+  } else if (banner.vfJsBannerState === 'dismissible') {
+    // nothing more to do for dismissible
+
+  } else if (banner.vfJsBannerState === 'blocking') {
+    console.warn('vf-banner: Note, the blocking implementation is not yet feature complete.');
+    // escape only works when blocking
+    if (banner.vfJsBannerEscClose === 'y' || banner.vfJsBannerEscClose === 'Y') {
+      document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        if (evt.keyCode == 27) {
+          vfBannerConfirm(targetBanner,'null');
+        }
+      };
+    }
+  }
+
+  // Split passed links into buttons
+  // <a href='#'>string1</a>\<a href='#'>string2</a>
+  if (banner.vfJsBannerExtraButton) {
+    var vfBannerExtraButtons = banner.vfJsBannerExtraButton.split('</a>');
+
+    vfBannerExtraButtons.forEach(function(button){
+      if (button.length > 1) {
+        button += '</a>';
+        var newButton = document.createElement('button');
+        newButton.innerHTML = button;
+        newButton = newButton.firstChild;
+        newButton.classList = 'vf-button vf-button--primary';
+        generatedBannerHtml += newButton.outerHTML;
+      }
+    });
+  }
+
+  // if there is a vfJsBannerButtonText and banner is blocking or dismissible,
+  // add a button so user can close the banner
+  if (banner.vfJsBannerButtonText && (banner.vfJsBannerState === 'blocking' || banner.vfJsBannerState === 'dismissible')) {
+    generatedBannerHtml += '<button class="vf-button vf-button--secondary" data-vf-js-banner-close>'+banner.vfJsBannerButtonText+'</button>';
+  }
+
+  generatedBannerHtml += '</div>';
+
+  // set the html of the banner
+  targetBanner.innerHTML = generatedBannerHtml;
+
+  // if blocking or dismissible, allow the user to close it, store a cookie (if specified)
+  if (banner.vfJsBannerState === 'blocking' || banner.vfJsBannerState === 'dismissible') {
+
+    // prep for cookie
+    var vfBannerCookieNameAndVersion = 'null';
+    if (banner.vfJsBannerCookieName && banner.vfJsBannerCookieVersion) {
+      vfBannerCookieNameAndVersion = banner.vfJsBannerCookieName + '_' + banner.vfJsBannerCookieVersion;
+
+      // console.warn('vf-banner: vfBannerReset cookie reset override is on.');
+      // vfBannerReset(vfBannerCookieNameAndVersion);
+
+      if (vfBannerGetCookie(vfBannerCookieNameAndVersion) === 'true') {
+        // banner has been accepted, close
+        targetBanner.classList += " vf-u-display-none";
+        // exit, nothng more to do
+        return;
+      }
+    }
+
+    // On click: close banner, pass any cooke name (or `null`)
+    if (banner.vfJsBannerButtonText) {
+      targetBanner.addEventListener('click', function(){
+        vfBannerConfirm(targetBanner,vfBannerCookieNameAndVersion);
+      }, false);
+    }
+  }
+}
+
+vfBanner();
+
+
+// By default this creates banners from HTML
+// optionally you can programatically supply
+// Target HTML
+// `<div class="vf-banner vf-banner--fixed vf-banner--bottom vf-banner--notice"
+//       data-vf-js-banner
+//       data-vf-js-banner-id="32423"
+//
+// ></div>`
+// var programaticalBanner = {
+//   vfJsBanner: "",
+//   vfJsBannerButtonText: "I agree, dismiss this banner",
+//   vfJsBannerCookieName: "MyService",
+//   vfJsBannerCookieVersion: "0.1",
+//   vfJsBannerExtraButton: "<a href='#'>Optional button</a><a target='_blank' href='#'>New tab button</a>",
+//   vfJsBannerId: "2352286",
+//   vfJsBannerText: '<p class="vf-text vf-text--body-l">This website uses cookies, and the limiting processing of your personal data to function. By using the site you are agreeing to this as outlined in our <a class="vf-link" href="JavaScript:Void(0);">Privacy Notice</a> and <a class="vf-link" href="JavaScript:Void(0);">Terms Of Use</a>.</p>',
+//   vfJsBannerState: "dismissible"
+// };
+// vfBannerInsert(programaticalBanner,'32423');
