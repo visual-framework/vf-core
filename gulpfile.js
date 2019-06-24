9151289558b5ddf5218305b23ab6865dd0a853be
Ken Hawkins <khawkins98@gmail.com>
2019-06-24 11:08:42 +0200

    Typo

--- gulpfile.js
+++ gulpfile.js
@@ -146,7 +146,7 @@ gulp.task('vf-css', function() {
   return gulp
     .src([componentPath+'/**/*.scss',componentPath+'/**/**/*.scss'], {
       allowEmpty: true,
-      ignore: [componentPath+'/**/index.scss',componentPath+'/**/**/index.scss','!'+componentPath+'/vf-core-components/vf-core/components/**/*.scss']
+      ignore: [componentPath+'/**/index.scss',componentPath+'/**/**/index.scss',componentPath+'/vf-core-components/vf-core/components/**/*.scss']
     })
     .pipe(ListStream.obj(function (err, data) {
       if (err)
