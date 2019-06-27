a6829db03b4ad4c6db444c47fdc5051286847c8d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-27 14:15:47 +0000

    Deploy visual-framework/vf-core to github.com/visual-framework/vf-core.git:gh-pages

--- components/raw/embl-conditional-edit/embl-conditional-edit.js
+++ components/raw/embl-conditional-edit/embl-conditional-edit.js
@@ -3,6 +3,7 @@
 /**
  * Invoke emblConditionalEditDetectParam scopped to objects where
  * data-embl-js-conditional-edit is present
+ * This will be dynamically run once emblContentHubSignalFinished is triggered.
  */
 function emblConditionalEdit() {
   const emblConditionalEdit = document.querySelectorAll('[data-embl-js-conditional-edit]');
@@ -21,7 +22,7 @@ function emblConditionalEdit() {
 }
 
 /**
- * Detects `?embl-conditional-edit=enabled` or `?embl-conditional-edit=1`
+ * Detects `?embl-conditional-edit=enabled` or `?embl-conditional-edit=1` or ?embl-conditional-edit=true`
  * and adds `.embl-coditional-edit__enabled` to display the edit links
  * @param {string} [url] - the url to check for an enabling param
  * @param {element} [element] - the scopped element to be processed
@@ -40,7 +41,7 @@ function emblConditionalEditDetectParam(url,element) {
   }
   captured = captured[1];
 
-  if (captured == '1' || captured == 'enabled') {
+  if (captured == '1' || captured == 'enabled' || captured == 'true') {
     element.className += ' ' + 'embl-coditional-edit__enabled';
   }
 }
