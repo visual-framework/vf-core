fb0661f1dd1c0b84bb0a24d0a29870f00a9799a4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-01 14:15:32 +0000

    Deploy visual-framework/vf-core to github.com/visual-framework/vf-core.git:gh-pages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -652,8 +652,55 @@ function emblContentHubLoaderHtmlImports() {
 	window["addImport"] = AddImport;
 }
 
-// embl-content-hub-loader__fetch
+// embl-conditional-edit
 
+/**
+ * Invoke emblConditionalEditDetectParam scopped to objects where
+ * data-embl-js-conditional-edit is present
+ * This will be dynamically run once emblContentHubSignalFinished is triggered.
+ */
+function emblConditionalEdit() {
+  const emblConditionalEditItems = document.querySelectorAll('[data-embl-js-conditional-edit]');
+  if (!emblConditionalEditItems) {
+    // exit: lists not found
+    return;
+  }
+  if (emblConditionalEditItems.length == 0) {
+    // exit: lists not found
+    return;
+  }
+
+  Array.prototype.forEach.call(emblConditionalEditItems, (element, i) => {
+    emblConditionalEditDetectParam(location.href,element);
+  });
+}
+
+/**
+ * Detects `?embl-conditional-edit=enabled` or `?embl-conditional-edit=1` or ?embl-conditional-edit=true`
+ * and adds `.embl-coditional-edit__enabled` to display the edit links
+ * @param {string} [url] - the url to check for an enabling param
+ * @param {element} [element] - the scopped element to be processed
+ */
+function emblConditionalEditDetectParam(url,element) {
+  var captured = /embl-conditional-edit=([^&]+)/.exec(url);
+  if (captured == null) {
+    // value not found
+
+    // also try against any parent iframe url
+    if (window.self !== window.top) {
+      emblConditionalEditDetectParam(parent.window.location,element);
+    }
+
+    return;
+  }
+  captured = captured[1];
+
+  if (captured == '1' || captured == 'enabled' || captured == 'true') {
+    element.className += ' ' + 'embl-coditional-edit__enabled';
+  }
+}
+
+// embl-content-hub-loader__fetch
 
 /**
  * Fetch html links from content.embl.org
@@ -744,6 +791,11 @@ function emblContentHubFetch() {
 
   // Show the remote content
   function emblContentHubGrabTheContent(targetLink,position,exportedContent) {
+
+    if (!exportedContent) {
+      console.log('No content found for this import, exiting. The import may have already been preformed.', targetLink);
+      return;
+    }
 
     // pickup the "meat" of the exported content
     exportedContent = exportedContent || targetLink.import.querySelector('.vf-content-hub-html');
@@ -864,54 +916,6 @@ function emblContentHub() {
 
   // 2. import the content
   emblContentHubFetch();
-}
-
-// embl-conditional-edit
-
-/**
- * Invoke emblConditionalEditDetectParam scopped to objects where
- * data-embl-js-conditional-edit is present
- * This will be dynamically run once emblContentHubSignalFinished is triggered.
- */
-function emblConditionalEdit$1() {
-  const emblConditionalEdit = document.querySelectorAll('[data-embl-js-conditional-edit]');
-  if (!emblConditionalEdit) {
-    // exit: lists not found
-    return;
-  }
-  if (emblConditionalEdit.length == 0) {
-    // exit: lists not found
-    return;
-  }
-
-  Array.prototype.forEach.call(emblConditionalEdit, (element, i) => {
-    emblConditionalEditDetectParam(location.href,element);
-  });
-}
-
-/**
- * Detects `?embl-conditional-edit=enabled` or `?embl-conditional-edit=1` or ?embl-conditional-edit=true`
- * and adds `.embl-coditional-edit__enabled` to display the edit links
- * @param {string} [url] - the url to check for an enabling param
- * @param {element} [element] - the scopped element to be processed
- */
-function emblConditionalEditDetectParam(url,element) {
-  var captured = /embl-conditional-edit=([^&]+)/.exec(url);
-  if (captured == null) {
-    // value not found
-
-    // also try against any parent iframe url
-    if (window.self !== window.top) {
-      emblConditionalEditDetectParam(parent.window.location,element);
-    }
-
-    return;
-  }
-  captured = captured[1];
-
-  if (captured == '1' || captured == 'enabled' || captured == 'true') {
-    element.className += ' ' + 'embl-coditional-edit__enabled';
-  }
 }
 
 // embl-content-meta-properties
