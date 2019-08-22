b73c29777f4d762e459863febf8749a71a3a4efc
Ken Hawkins <khawkins98@gmail.com>
2019-08-22 10:52:27 +0200

    Bug: vf-watch shouldn't reload on package.variables.scss generation

--- tools/gulp-tasks/vf-watch.js
+++ tools/gulp-tasks/vf-watch.js
@@ -7,7 +7,7 @@
 
 module.exports = function(gulp, path, componentPath, reload) {
   return gulp.task('vf-watch', function(done) {
-    gulp.watch(componentPath + '/**/*.scss', gulp.series('vf-css')).on('change', reload);
+    gulp.watch([componentPath + '/**/*.scss', '!' + componentPath + '/**/package.variables.scss'], gulp.series('vf-css')).on('change', reload);
     gulp.watch(componentPath + '/**/*.js', gulp.series('vf-scripts')).on('change', reload);
     gulp.watch(componentPath + '/**/**/assets/*.svg', gulp.series('vf-svg','vf-component-assets')).on('change', reload);
     gulp.watch([componentPath + '/**/**/assets/*', '!' + componentPath + '/**/**/assets/*.svg'], gulp.series('vf-component-assets')).on('change', reload);
