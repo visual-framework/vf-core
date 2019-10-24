52703e70829e5c5b45cb4af64119d25a7e3ebc97
Ken Hawkins <khawkins98@gmail.com>
2019-10-24 13:15:22 +0200

    Merge develop in

--- tools/vf-config/index.js
+++ tools/vf-config/index.js
@@ -40,6 +40,11 @@ global.vfComponentPath = config.vfConfig.vfComponentPath || path.resolve('.', 'c
 global.vfBuildDestination = config.vfConfig.vfBuildDestination || 'temp/build-files';
 global.vfThemePath = config.vfConfig.vfThemePath || './tools/vf-frctl-theme';
 global.vfVersion = vfCoreConfig.version || 'not-specified';
+global.vfBuildFractalMode = vfCoreConfig.vfConfig.vfBuildFractalMode || 'normal'; 
+  // in vf-build specifices to run fractal as 'normal' full build of static assets,
+  // 'dataobject' to render only to memory or
+  // 'none' to not build at all
+
 const componentPath = path.resolve('.', global.vfComponentPath).replace(/\\/g, '/');
 const componentDirectories = config.vfConfig.vfComponentDirectories || ['vf-core-components'];
 const buildDestionation = path.resolve('.', global.vfBuildDestination).replace(/\\/g, '/'); 
