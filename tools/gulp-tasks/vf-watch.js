a72e2b655d9dbf35b030ddd3234ec227af434ac8
Ken Hawkins <khawkins98@gmail.com>
2019-10-25 13:49:31 +0200

    Bug: fix nunjucks compilation loop
    The Nunjucks precompilation watch task can get stuck in a loop due to some strange behaviour around the file being read, renamed and written differently.
    
    `usePolling` provides a more accurate behaviour.

--- tools/gulp-tasks/vf-watch.js
+++ tools/gulp-tasks/vf-watch.js
@@ -9,8 +9,11 @@ module.exports = function(gulp, path, componentPath, reload) {
   return gulp.task('vf-watch', function(done) {
     gulp.watch([componentPath + '/**/*.scss', '!' + componentPath + '/**/package.variables.scss'], gulp.series('vf-css')).on('change', reload);
     gulp.watch([componentPath + '/**/*.js', '!' + componentPath + '/**/*.precompiled.js'], gulp.series('vf-scripts')).on('change', reload);
-    gulp.watch([componentPath + '/**/*.njk'], gulp.series('vf-templates-precompile'));
+    gulp.watch([componentPath + '/**/*.njk'], {
+      usePolling: true // uses fs.watchFile(), otherwise this gets stuck in a loop
+    }, gulp.series('vf-templates-precompile'));
     gulp.watch([componentPath + '/**/**/assets/*'], gulp.series('vf-component-assets')).on('change', reload);
+
   });
 
 };
