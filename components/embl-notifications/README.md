68df36b0af164550eb0abd64ffd08f5682c07ef8
Ken Hawkins <khawkins98@gmail.com>
2020-03-04 10:02:12 +0100

    Add work plan and stub buildout

--- components/embl-notifications/README.md
+++ components/embl-notifications/README.md
@@ -2,6 +2,8 @@
 
 [![npm version](https://badge.fury.io/js/%40visual-framework%2Fembl-notifications.svg)](https://badge.fury.io/js/%40visual-framework%2Fembl-notifications)
 
+Fetches notifications from the EMBL contentHub and will display them, if a matching URL is found.
+
 ## Install
 
 This repository is distributed with [npm][npm]. After [installing npm][install-npm], you can install `embl-notifications` with this command.
@@ -12,10 +14,11 @@ $ yarn add --dev @visual-framework/embl-notifications
 
 ## Usage
 
-The source files included are written in [Sass][sass] (`scss`) You can simply point your sass `include-path` at your `node_modules` directory and import it like this.
+Add this to your `./components/vf-component-rollup/scripts.js`:
 
-```
-@import "@visual-framework/embl-notifications/index.scss";
+```js
+import { vfEmblNotification } from '../components/raw/embl-notifications/embl-notifications.js';
+emblNotifications(); // invoke it
 ```
 
-_Make sure you import Sass requirements along with the modules._
+
--- components/embl-notifications/embl-notifications.js
+++ components/embl-notifications/embl-notifications.js
@@ -17,28 +17,37 @@
  * to drop `data-vf-js-component` and still maintain CSS styling.
  */
 
-// Uncomment this boilerplate
-// // if you need to import any other components' JS to use here
-// import { vfOthercomponent } from 'vf-other-component/vf-other-component';
-//
-//  /**
-//   * The global function for this component
-//   * @example vfcomponentName(firstPassedVar)
-//   * @param {string} [firstPassedVar]  - An option to be passed
-//   */
-// function vfcomponentName(firstPassedVar) {
-//   firstPassedVar = firstPassedVar || 'defaultVal';
-//
-// }
-//
-// // If you need to invoke the component by default
-// vfcomponentName();
-//
-// // By default your component should be usable with js imports
-// export { vfcomponentName };
-//
-// // You should also import it at ./components/vf-core/scripts.js
-// // import { vfcomponentName } from '../components/raw/vf-component/vf-component.js';
-// // And, if needed, invoke it
-// // vfcomponentName();
+// if you need to import any other components' JS to use here
+import { vfBanner } from 'vf-banner/vf-banner';
+
+ /**
+  * The global function for this component
+  * @example emblNotifications(firstPassedVar)
+  * @param {string} [firstPassedVar]  - An option to be passed
+  */
+function emblNotifications(firstPassedVar) {
+  firstPassedVar = firstPassedVar || 'defaultVal';
+
+  // @todo
+  // 1. Fetch json from contentHub
+  // 2. If success
+  //   - get current page url
+  //   - compare to `notification_ulrs`
+  //     We can steal code from the EBI VF https://github.com/ebiwd/EBI-Framework/blob/v1.3/js/ebi-global-includes/script/4_ebiFrameworkContent.js#L391-L475
+  // 3. If matching URL
+  //   - invoke vf-banner
+  //   - using options from `notification_position` `notification_presentation` `body` `title` `notification_link`
+
+  // Possible features not currently planned:
+  // - Only show if a wrapping element has data-vf-js-embl-notifications`
+
+}
+
+// By default your component should be usable with js imports
+export { emblNotifications };
+
+// Add this to your ./components/vf-component-rollup/scripts.js
+// import { vfEmblNotification } from '../components/raw/embl-notifications/embl-notifications.js';
+// And invoke it
+// emblNotifications();
 
