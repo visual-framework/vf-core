116b31bbe598ec23c230cbb39f5ad3597ea10b31
Stu Robson <stu@alwaystwisted.com>
2020-03-30 09:40:15 +0100

    removes floatlabel from forms

--- components/vf-componenet-rollup/scripts.js
+++ components/vf-componenet-rollup/scripts.js
@@ -14,16 +14,13 @@ import { vfMastheadSetStyle } from 'vf-masthead/vf-masthead';
 vfMastheadSetStyle();
 
 import { vfGaIndicateLoaded } from 'vf-analytics-google/vf-analytics-google';
-vfGaIndicateLoaded(); 
+vfGaIndicateLoaded();
 
 import { vfTabs } from 'vf-tabs/vf-tabs';
 vfTabs();
 
 import { vfTree } from 'vf-tree/vf-tree';
 vfTree();
-
-import { vfFormFloatLabels } from 'vf-form__core/assets/vf-form__float-labels.js';
-vfFormFloatLabels();
 
 // All EMBL JS
 import { emblContentHubLoaderHtmlImports } from 'embl-content-hub-loader/embl-content-hub-loader__html-imports';
