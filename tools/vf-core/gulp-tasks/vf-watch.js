9efa19bca721cdf0cde025477ec392b7c82b7d72
Stu Robson <stu@alwaystwisted.com>
2020-08-17 11:29:37 +0100

    adds an interval for all gulp watch tasks so that the computer CPU doesn't go crazy

--- tools/vf-core/gulp-tasks/vf-watch.js
+++ tools/vf-core/gulp-tasks/vf-watch.js
@@ -7,11 +7,12 @@
 
 module.exports = function(gulp, path, componentPath, reload) {
   return gulp.task('vf-watch', function(done) {
-    gulp.watch([componentPath + '/**/*.scss', '!' + componentPath + '/**/package.variables.scss'], gulp.series('vf-css')).on('change', reload);
-    gulp.watch([componentPath + '/**/*.js', '!' + componentPath + '/**/*.precompiled.js'], gulp.series('vf-scripts')).on('change', reload);
+    gulp.watch([componentPath + '/**/*.scss', '!' + componentPath + '/**/package.variables.scss'], { interval: 500 }, gulp.series('vf-css')).on('change', reload);
+    gulp.watch([componentPath + '/**/*.js', '!' + componentPath + '/**/*.precompiled.js'], { interval: 500 }, gulp.series('vf-scripts')).on('change', reload);
     gulp.watch([componentPath + '/**/*.njk'], {
-      usePolling: true // uses fs.watchFile(), otherwise this gets stuck in a loop
+      usePolling: true, // uses fs.watchFile(), otherwise this gets stuck in a loop
+      interval: 500
     }, gulp.series('vf-templates-precompile'));
-    gulp.watch([componentPath + '/**/**/assets/*'], gulp.series('vf-component-assets')).on('change', reload);
+    gulp.watch([componentPath + '/**/**/assets/*'], { interval: 500 }, gulp.series('vf-component-assets')).on('change', reload);
   });
 };
