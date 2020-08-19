55c5b4910a018f4f87e892b3d780bba806aa899c
Ken Hawkins <khawkins98@gmail.com>
2020-08-19 14:03:26 +0200

    Get child component listings working

--- tools/vf-component-library/src/site/_data/fractalEnvironment.js
+++ tools/vf-component-library/src/site/_data/fractalEnvironment.js
@@ -18,17 +18,19 @@
 
 let localComponents = global.fractal.components.toJSON().items;
 
-// Filter through the components to select only the vf-core-components object
-// function filterVfCoreComponents(item) {
-//   return item.handle == 'vf-core-components';
-// }
-
-// let vfCoreComponents = localComponents.find(filterVfCoreComponents)
-// console.log(vfCoreComponents)
+// Filter through the components to make a list of components with children components, like vf-form
+let parentComponents = localComponents.filter(component => component.items);
+let childComponents = [];
+parentComponents.forEach(parent => {
+  parent.items.forEach(component => {
+    childComponents.push(component);
+  });
+});
 
 module.exports = {
   fractal: global.fractal,
   localComponents: localComponents,
+  childComponents: childComponents
   // vfCoreComponents: vfCoreComponents.items
   // coreComponents: global.fractal.components.items()
 };
