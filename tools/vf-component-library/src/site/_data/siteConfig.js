f371b0c4d8f2e5d205d93ea7bacc49796f78463a
Ken Hawkins <khawkins98@gmail.com>
2020-10-05 12:35:50 +0200

    Homepage formatting

--- tools/vf-component-library/src/site/_data/siteConfig.js
+++ tools/vf-component-library/src/site/_data/siteConfig.js
@@ -3,7 +3,7 @@ const { DateTime } = require('luxon');
 // Some various reusable configuration
 module.exports = {
   siteInformation: {
-    title: "Visual Framework 2.0",
+    title: "The Visual Framework 2.0",
     short_description: 'A front-end toolkit to quickly build better life science websites.',
     url: "https://visual-framework.github.io/vf-core/",
     author: "Visual Framework system",
