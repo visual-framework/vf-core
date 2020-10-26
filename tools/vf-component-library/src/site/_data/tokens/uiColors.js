a1f4a917fb3bd4ee1e477f09ffa96db9bc76e93f
sturobson <stu@alwaystwisted.com>
2020-10-26 11:04:21 +0000

    adds JS files to pull in the json from the design tokens pacakge

---
+++ tools/vf-component-library/src/site/_data/tokens/uiColors.js
@@ -0,0 +1,3 @@
+const path = require('path');
+const pkg = require(path.join(process.cwd(), 'node_modules/@visual-framework/vf-design-tokens/dist/json/vf-ui-colors.ios.json'));
+module.exports = pkg;
