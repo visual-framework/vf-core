51ce076e2c813e6c01c466693e9086138a5424d3
robson <stu.robson@embl.de>
2021-02-10 09:05:41 +0000

    adds js things that were / are needed

---
+++ tools/vf-component-library/src/site/_data/components/button.js
@@ -0,0 +1,4 @@
+
+const path = require('path');
+const pkg = require(path.join(process.cwd(), 'node_modules/@visual-framework/vf-design-tokens/dist/components/vf-button.ios.json'));
+module.exports = pkg;
