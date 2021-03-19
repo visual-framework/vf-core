86d936f8569e8529df54ba30c9701b002a5afc6a
robson <stu.robson@embl.de>
2021-03-19 15:55:51 +0000

    adds neutral color tokens

--- components/vf-design-tokens/gulpfile.js
+++ components/vf-design-tokens/gulpfile.js
@@ -90,6 +90,7 @@ gulp.task("tokens:maps", () =>
 gulp.task("tokens:json", () =>
   gulp.src([
     "./src/maps/vf-colors.yml",
+    "./src/maps/vf-color__neutral.yml",
     "./src/maps/vf-spacing.yml",
     "./src/maps/vf-themes.yml",
     "./src/maps/vf-ui-colors.yml",
