49b7a7bb8a657acaeeabae20920dd0d4d00ca94f
robson <stu.robson@embl.de>
2021-03-21 15:11:09 +0000

    adds neutral tokens page

--- tools/vf-component-library/CHANGELOG.md
+++ tools/vf-component-library/CHANGELOG.md
@@ -1,3 +1,7 @@
+### 1.3.0
+
+* adds neutral colour tokens
+
 ### 1.2.0
 
 * Adds the form options as their own set of components in the list.
---
+++ tools/vf-component-library/src/site/_data/tokens/neutral_colours.js
@@ -0,0 +1,3 @@
+const path = require('path');
+const pkg = require(path.join(process.cwd(), 'node_modules/@visual-framework/vf-design-tokens/dist/json/vf-color__neutral.ios.json'));
+module.exports = pkg;
