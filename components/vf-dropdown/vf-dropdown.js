e2eccd163aaf0662b825e25d34f9cfb620b8c157
Ken Hawkins <khawkins98@gmail.com>
2021-09-29 16:24:31 +0200

    chore: drop-down and progress component cleanup (#1684)
    * chore: drop-down and progress component cleanup
    
    Following work in #1682 and #1679 to introduce the new alpha components this cleans up a pair of rough edges.
    
    - css custom prop usage
    - ie11 fallback
    - njk typo
    - njk whitespace control
    - add aria progress  bar role
      - maybe more work to do there in the future https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_progressbar_role
    - add context conditionals
    
    * Update vf-progress-indicator.jsx
    
    * CSS linting and nesting
    
    * More linting
    
    * Use z-index tokens, context
    
    * Small typo fixes
    
    * Remove unused event handler

--- components/vf-dropdown/vf-dropdown.js
+++ components/vf-dropdown/vf-dropdown.js
@@ -3,15 +3,14 @@
 /**
  * Function for JS scroll to top functionality
  * That must be executed exactly once
- * @example vfBackToTop()
+ * @example vfDropdown()
  */
 export function vfDropdown() {
   const components = document.querySelectorAll("[data-vf-js-dropdown]");
 
   for (let component of components) {
-    component.addEventListener("click", (event) => {
+    component.addEventListener("click", () => {
       component.classList.toggle("vf-dropdown--open");
     });
   }
-
 }
