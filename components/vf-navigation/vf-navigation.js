e9268d521eb4a9f877e87a6169d065e6d02e6767
Ken Hawkins <khawkins98@gmail.com>
2021-10-29 20:23:01 +0200

    feature: vf-navigation--on-this-page
    Introduces a refined `vf-navigation--on-this-page` that has been trialed on https://wwwdev.embl.org/directory/ and will replace `vf-links-list-easy`
    
    Adresses: #1623

---
+++ components/vf-navigation/vf-navigation.js
@@ -0,0 +1,43 @@
+// only required for vf-navigation--on-this-page
+// scrollspy concept ported from https://codepen.io/zchee/pen/ogzvZZ
+
+// vf-navigation
+
+/**
+ * Function for JS scroll to top functionality
+ * That must be executed exactly once
+ * @example vfNavigationOnThisPage()
+ */
+export function vfNavigationOnThisPage() {
+  // const components = document.querySelectorAll("[data-vf-js-dropdown]");
+
+  // for (let component of components) {
+  //   component.addEventListener("click", () => {
+  //     component.classList.toggle("vf-dropdown--open");
+  //   });
+  // }
+
+  // to do: make this not explode if sectionList missing
+  var sectionList = document.querySelectorAll("[data-vf-js-scroll-activate-container]")[0];
+  var section = document.querySelectorAll("[data-vf-js-scroll-activate]");
+  var sectionPositions = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sectionPositions[e.id] = e.offsetTop;
+  });
+
+  window.onscroll = function() {
+    // to do: throttle and request animationframe
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    for (i in sectionPositions) {
+      if (sectionPositions[i] <= scrollPosition) {
+        // only add remove the class if there is a new section to activate
+        if (sectionList.querySelector("a[href*=" + i + "]") != null) {
+          sectionList.querySelector(".active").classList.remove("active");
+          sectionList.querySelector("a[href*=" + i + "]").classList.add("active");
+        }
+      }
+    }
+  };
+}
