4cc40dd36c00405fd8a23ffd0e1c905f4a419ac6
Ken Hawkins <khawkins98@gmail.com>
2021-11-23 11:22:12 +0100

    Prepare release notes

--- tools/vf-component-library/src/site/_data/siteConfig.js
+++ tools/vf-component-library/src/site/_data/siteConfig.js
@@ -15,7 +15,7 @@ module.exports = {
   // vfVersion: "develop",
   // vfVersionPrefix: "dev.",
   // use this when tagging a release
-  vfVersion: "v2.5.4",
+  vfVersion: "v2.5.5",
   vfVersionPrefix: "",
   sections: {
     about: {
