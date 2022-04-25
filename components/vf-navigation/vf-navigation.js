9ed59f45dc47a91e01d9aa566286961d2c9b451f
Ken Hawkins <khawkins98@gmail.com>
2022-04-25 09:26:25 +0200

    Make vf-navigation--on-this-page more dynamic
    - allows for multiple on this page navigators
    - activates the indicator only when the content is in focus
    - is a breaking change! (requires a `data-vf-js-navigation-on-this-page-link` on the navigation links)
    
    I think this is the *better* approach, but it's also disruptive as it has a breaking change.

--- components/vf-navigation/vf-navigation.js
+++ components/vf-navigation/vf-navigation.js
@@ -6,10 +6,16 @@
  * That must be executed exactly once
  * @example vfNavigationOnThisPage()
  */
+
 export function vfNavigationOnThisPage() {
-  var sectionList = document.querySelectorAll("[data-vf-js-navigation-on-this-page-container='true'],[data-vf-js-navigation-on-this-page-container='1']")[0];
-  var section = document.querySelectorAll("[id]");
+  var scope = document;
+  var sectionList = scope.querySelectorAll(
+    "[data-vf-js-navigation-on-this-page-link]"
+  );
+  // console.log(sectionList);
+  var section = scope.querySelectorAll("[id]");
   var sectionPositions = {};
+  var sectionHeights = {};
   var i = 0;
 
   if (!sectionList || !section) {
@@ -21,26 +27,43 @@ export function vfNavigationOnThisPage() {
     return;
   }
 
-  section.forEach(e => {
+  section.forEach((e) => {
     sectionPositions[e.id] = e.offsetTop;
+  });
+  section.forEach((e) => {
+    sectionHeights[e.id] = e.offsetHeight;
   });
 
   function activateNavigationItem() {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    var scrollPosition =
+      document.documentElement.scrollTop || document.body.scrollTop;
     for (i in sectionPositions) {
       // this implements a scrollspy concept based on https://codepen.io/zchee/pen/ogzvZZ
-      if (sectionPositions[i] <= scrollPosition + 70) {
+      if (sectionPositions[i] <= scrollPosition + 0) {
         // we activate the section 70 pixels before coming into view, as the sticky bar will cover it
         // only add remove the class if there is a new section to activate
-        if (sectionList.querySelector("a[href*='" + i + "']") != null) {
-          sectionList.querySelector("[aria-selected]").removeAttribute("aria-selected");
-          sectionList.querySelector("a[href*='" + i + "']").setAttribute("aria-selected", "true");
-        }
+        sectionList.forEach(function (currentValue, currentIndex, listObj) {
+          if (currentValue.href.includes(i)) {
+            currentValue.setAttribute("aria-selected", "true");
+          }
+        }, "myThisArg");
+      }
+      if (
+        sectionPositions[i] - scrollPosition <=
+          -Math.abs(window.innerHeight + sectionHeights[i] - 70) ||
+        sectionPositions[i] - scrollPosition >= window.innerHeight
+      ) {
+        // we activate the container only while it is in view
+        sectionList.forEach(function (currentValue, currentIndex, listObj) {
+          if (currentValue.href.includes(i)) {
+            currentValue.setAttribute("aria-selected", "false");
+          }
+        }, "myThisArg");
       }
     }
   }
 
-  window.onscroll = function() {
+  window.onscroll = function () {
     // we could introduce throttling, but as this is a fairly simple repaint, throttling is not likely required
     window.requestAnimationFrame(activateNavigationItem);
   };
