02328205e8da688a1a10540580aebd0111b10a8b
Francisco Peña <francisco@ebi.ac.uk>
2022-08-08 14:35:59 +0100

    feat: improve logic for scrollspy, remove breaking change

--- components/vf-navigation/vf-navigation.js
+++ components/vf-navigation/vf-navigation.js
@@ -9,58 +9,48 @@
 
 export function vfNavigationOnThisPage() {
   var scope = document;
-  var sectionList = scope.querySelectorAll(
-    "[data-vf-js-navigation-on-this-page-link]"
+  var navLinks = scope.querySelectorAll(
+    "[data-vf-js-navigation-on-this-page-container] .vf-navigation__item a"
   );
-  // console.log(sectionList);
-  var section = scope.querySelectorAll("[id]");
-  var sectionPositions = {};
-  var sectionHeights = {};
-  var i = 0;
 
-  if (!sectionList || !section) {
+  var sections = scope.querySelectorAll("[id]");
+  var sectionPositions = [];
+
+  if (!navLinks || !sections) {
     // exit: either sections or section content not found
     return;
   }
-  if (sectionList.length === 0 || section.length === 0) {
+  if (navLinks.length === 0 || sections.length === 0) {
     // exit: either sections or section content not found
     return;
   }
 
   function activateNavigationItem() {
     // althought costly, we recalculate the position of elements each time as things move or load dynamically
-    section.forEach((e) => {
+    sectionPositions = [];
+    sections.forEach((e) => {
       var rect = e.getBoundingClientRect();
       var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      sectionPositions[e.id] = rect.top + scrollTop;
-      sectionHeights[e.id] = e.offsetHeight;
+      // as currently we query for all elements with id, we remove script tags and other elements
+      if (rect.width !== 0 && rect.height !== 0) {
+        sectionPositions.push({ id: e.id, position: rect.top + scrollTop});
+      }
     });
+    sectionPositions.reverse();
 
-    var scrollPosition =
-      document.documentElement.scrollTop || document.body.scrollTop;
-    for (i in sectionPositions) {
-      // this implements a scrollspy concept based on https://codepen.io/zchee/pen/ogzvZZ
-      if (sectionPositions[i] <= scrollPosition + 0) {
-        // we activate the section 70 pixels before coming into view, as the sticky bar will cover it
-        // only add remove the class if there is a new section to activate
-
-        sectionList.forEach(function (currentValue, currentIndex, listObj) {
-          // console.log(currentValue.href);
-          if (currentValue.href.includes(i)) {
-            currentValue.setAttribute("aria-selected", "true");
-          }
-        }, "myThisArg");
-      }
-      var isNotYetOnScreen = sectionPositions[i] > scrollPosition;
-      var bottomOfElement = sectionPositions[i] + sectionHeights[i];
-      if (scrollPosition - 70 > bottomOfElement || isNotYetOnScreen) {
-        // we activate the container only while it is in view
-        sectionList.forEach(function (currentValue, currentIndex, listObj) {
-          if (currentValue.href.includes(i)) {
-            currentValue.setAttribute("aria-selected", "false");
-          }
-        }, "myThisArg");
-      }
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    var currentSection = sectionPositions.find(function(s) { return !(scrollPosition <= s.position - 95) });
+    navLinks.forEach(function(link) {
+      link.setAttribute("aria-selected", "false")
+    });
+    if (!currentSection) {
+      navLinks[0].setAttribute("aria-selected", "true")
+    } else {
+      navLinks.forEach(function(link) {
+        if(link.hash === ('#'+currentSection.id)) {
+          link.setAttribute("aria-selected", "true");
+        }
+      })
     }
     isCalculating = false;
   }
@@ -70,8 +60,22 @@ export function vfNavigationOnThisPage() {
     if (!isCalculating) {
       isCalculating = true;
       window.requestAnimationFrame(activateNavigationItem);
-    } else {
-      // console.log("throttled!");
     }
   };
+  navLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      var section = document.querySelector(link.hash);
+      var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+      if (!section) return;
+      event.preventDefault();
+      var elemStyles = window.getComputedStyle(section);
+      var value = elemStyles.paddingTop !== '0px' ? elemStyles.paddingTop : elemStyles.marginTop;
+      var offset = parseInt(value.slice(0, -2), 10);
+      window.scroll({
+        top: (section.getBoundingClientRect().top + scrollPosition) - (offset + 40),
+        behavior: 'smooth',
+      })
+    });
+  });
+
 }
