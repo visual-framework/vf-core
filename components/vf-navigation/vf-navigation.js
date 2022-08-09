41f5a0c77af869f3ffb85d860420ec79ac8c1f9a
Francisco Peña <francisco@ebi.ac.uk>
2022-08-09 15:36:14 +0100

    fix for navigation in wordpress content, add comments

--- components/vf-navigation/vf-navigation.js
+++ components/vf-navigation/vf-navigation.js
@@ -9,11 +9,19 @@
 
 export function vfNavigationOnThisPage() {
   var scope = document;
+  // based on the attribute we select all navigation links
   var navLinks = scope.querySelectorAll(
-    "[data-vf-js-navigation-on-this-page-container] .vf-navigation__item a"
+    "[data-vf-js-navigation-on-this-page-container='true'] .vf-navigation__item a"
   );
-
-  var sections = scope.querySelectorAll("[id]");
+  // we store all ids from anchor tags to know the sections we should care about
+  var ids = [];
+  navLinks.forEach(function(link) {
+    if(link.hash) {
+      ids.push(link.hash.substring(1));
+    }
+  });
+  // get all elements with an id and convert it from NodeList to Array
+  var sections = Array.prototype.slice.call(scope.querySelectorAll("[id]"));
   var sectionPositions = [];
 
   if (!navLinks || !sections) {
@@ -24,25 +32,30 @@ export function vfNavigationOnThisPage() {
     // exit: either sections or section content not found
     return;
   }
-
+  // remove all the elements that doesn't appear in the navigation based on it's id
+  sections = sections.filter(function(section) {
+    return ids.indexOf(section.id) !== -1;
+  });
   function activateNavigationItem() {
     // althought costly, we recalculate the position of elements each time as things move or load dynamically
     sectionPositions = [];
-    sections.forEach((e) => {
+    sections.forEach(function (e) {
       var rect = e.getBoundingClientRect();
       var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      // as currently we query for all elements with id, we remove script tags and other elements
-      if (rect.width !== 0 && rect.height !== 0) {
-        sectionPositions.push({ id: e.id, position: rect.top + scrollTop});
-      }
+      sectionPositions.push({ id: e.id, position: rect.top + scrollTop});
     });
+    // put sections in the bottom at the beginning of the array
     sectionPositions.reverse();
 
     var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    // We reverse the array because Array.find starts to search from position 0 and to simplify
+    // the logic to find which element is closest to the current scroll position, otherwise it will always
+    // select the first element.
     var currentSection = sectionPositions.find(function(s) { return !(scrollPosition <= s.position - 95) });
     navLinks.forEach(function(link) {
       link.setAttribute("aria-selected", "false")
     });
+    // if we don't match any section yet, highlight the first link
     if (!currentSection) {
       navLinks[0].setAttribute("aria-selected", "true")
     } else {
@@ -68,9 +81,13 @@ export function vfNavigationOnThisPage() {
       var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
       if (!section) return;
       event.preventDefault();
+      // get current styles of element we are moving to
       var elemStyles = window.getComputedStyle(section);
+      // take into account the padding and/or margin top
       var value = elemStyles.paddingTop !== '0px' ? elemStyles.paddingTop : elemStyles.marginTop;
+      // we remove the px characters from the value
       var offset = parseInt(value.slice(0, -2), 10);
+      // total offset: margin/padding top of the element plus the size of the navigation bar
       window.scroll({
         top: (section.getBoundingClientRect().top + scrollPosition) - (offset + 40),
         behavior: 'smooth',
