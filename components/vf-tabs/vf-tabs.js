05b24f8606de2f0bf17a1eab46878d2b44e4657d
Bhushan Palsapure <bhushanp@ebi.ac.uk>
2024-04-06 01:32:42 +0100

    Issue 1935: Fixes for lint errors seen during build.

--- components/vf-tabs/vf-tabs.js
+++ components/vf-tabs/vf-tabs.js
@@ -6,7 +6,7 @@
  * @param {boolean} [activateDeepLinkOnLoad] - if deep linked tabs should be activated on page load, defaults to true
  * @example vfTabs(document.querySelectorAll('.vf-component__container')[0]);
  */
- function vfTabs(scope, activateDeepLinkOnLoad) {
+function vfTabs(scope, activateDeepLinkOnLoad) {
   /* eslint-disable no-redeclare */
   var scope = scope || document;
   var activateDeepLinkOnLoad = activateDeepLinkOnLoad || true;
@@ -14,7 +14,9 @@
   // Get relevant elements and collections
   const tabsList = scope.querySelectorAll("[data-vf-js-tabs]");
   const panelsList = scope.querySelectorAll("[data-vf-js-tabs-content]");
-  const panels = scope.querySelectorAll("[data-vf-js-tabs-content] [id^=\"vf-tabs__section\"]");
+  const panels = scope.querySelectorAll(
+    "[data-vf-js-tabs-content] [id^='vf-tabs__section']"
+  );
   const tabs = scope.querySelectorAll("[data-vf-js-tabs] .vf-tabs__link");
   if (!tabsList || !panels || !tabs) {
     // exit: either tabs or tabbed content not found
@@ -43,9 +45,7 @@
     tab.addEventListener("click", e => {
       e.preventDefault();
       vfTabsSwitch(e.currentTarget, panels);
-    });    
-
-
+    });
 
     // Handle keydown events for keyboard users
     tab.addEventListener("keydown", e => {
@@ -53,18 +53,29 @@
       let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
       // Work out which key the user is pressing and
       // Calculate the new tab's index where appropriate
-      let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? "down" : null;
+      let dir =
+        e.which === 37
+          ? index - 1
+          : e.which === 39
+            ? index + 1
+            : e.which === 40
+              ? "down"
+              : null;
       if (dir !== null) {
         e.preventDefault();
         // If the down key is pressed, move focus to the open panel,
         // otherwise switch to the adjacent tab
-        dir === "down" ? panels[i].focus({preventScroll:true}) : tabs[dir] ? vfTabsSwitch(tabs[dir], panels) : void 0;
+        dir === "down"
+          ? panels[i].focus({ preventScroll: true })
+          : tabs[dir]
+            ? vfTabsSwitch(tabs[dir], panels)
+            : void 0;
       }
     });
   });
 
   // Add tab panel semantics and hide them all
-  Array.prototype.forEach.call(panels, (panel) => {
+  Array.prototype.forEach.call(panels, panel => {
     panel.setAttribute("role", "tabpanel");
     panel.setAttribute("tabindex", "-1");
     // let id = panel.getAttribute("id");
@@ -73,7 +84,7 @@
   });
 
   // Add the tabsList role to the first <ul> in the .tabbed container
-  Array.prototype.forEach.call(tabsList, (tabsListset) => {
+  Array.prototype.forEach.call(tabsList, tabsListset => {
     tabsListset.setAttribute("role", "tabsList");
     // Initially activate the first tab
     let firstTab = tabsListset.querySelectorAll(".vf-tabs__link")[0];
@@ -81,7 +92,7 @@
     firstTab.setAttribute("aria-selected", "true");
     firstTab.classList.add("is-active");
   });
-  Array.prototype.forEach.call(panelsList, (panel) => {
+  Array.prototype.forEach.call(panelsList, panel => {
     // Initially reveal the first tab panel
     let firstPanel = panel.querySelectorAll(".vf-tabs__section")[0];
     firstPanel.hidden = false;
@@ -97,7 +108,7 @@
 const vfTabsSwitch = (newTab, panels) => {
   // Update url based on tab id
   const data = newTab.getAttribute("id");
-  const url = '#' + data;
+  const url = "#" + data;
   window.history.replaceState(data, null, url);
 
   // get the parent ul of the clicked tab
@@ -110,14 +121,14 @@ const vfTabsSwitch = (newTab, panels) => {
 
     for (let item = 0; item < panels.length; item++) {
       const panel = panels[item];
-      if (panel.id === oldTab.id){
+      if (panel.id === oldTab.id) {
         panel.hidden = true;
         break;
       }
     }
   }
 
-  newTab.focus({preventScroll:true});
+  newTab.focus({ preventScroll: true });
   // Make the active tab focusable by the user (Tab key)
   newTab.removeAttribute("tabindex");
   // Set the selected state
@@ -127,7 +138,7 @@ const vfTabsSwitch = (newTab, panels) => {
   // tab panel to show
   for (let item = 0; item < panels.length; item++) {
     const panel = panels[item];
-    if (panel.id === newTab.id){
+    if (panel.id === newTab.id) {
       panel.hidden = false;
       break;
     }
@@ -136,7 +147,7 @@ const vfTabsSwitch = (newTab, panels) => {
 
 function vfTabsDeepLinkOnLoad(tabs, panels) {
   // 1. See if there is a `#vf-tabs__section--88888`
-  if(window.location.hash) {
+  if (window.location.hash) {
     var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
   } else {
     // No hash found
@@ -145,7 +156,7 @@ function vfTabsDeepLinkOnLoad(tabs, panels) {
   console.log("vfTabs: will activate tab", hash);
 
   // 2. loop through all tabs, if a match then activate
-  Array.prototype.forEach.call(tabs, (tab) => {
+  Array.prototype.forEach.call(tabs, tab => {
     let tabId = tab.getAttribute("data-tabs__item");
     if (tabId == hash) {
       vfTabsSwitch(tab, panels);
