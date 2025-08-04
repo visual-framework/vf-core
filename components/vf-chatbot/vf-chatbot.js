f30779cb27658820835a2cf7fe91417f40a4c491
Bhushan Palsapure <bhushanp@ebi.ac.uk>
2025-08-04 08:04:59 +0100

    VF Chatbot : Modal chatbot review changes

--- components/vf-chatbot/vf-chatbot.js
+++ components/vf-chatbot/vf-chatbot.js
@@ -60,12 +60,31 @@ VFChatbot.prototype = {
   }
 };
 
+// Utility to update bottom banner height for all chatbots
+function updateChatbotBottomMargin(heightPx = 0) {
+  document.querySelectorAll("[data-vf-js-chatbot]").forEach(element => {
+    element.style.setProperty("--vf-bottom-banner-height", `${heightPx}px`);
+  });
+}
+
 function getBottomBannerHeight(userHeight) {
   // If user provided a height, use it
   if (typeof userHeight === "number") return userHeight;
   // Otherwise, check for .vf-banner--bottom
   const banner = document.querySelector(".vf-banner--bottom");
   if (banner) {
+    // If your banner emits a custom event on close, listen for it:
+    banner.addEventListener("vf-banner:close", () => {
+      updateChatbotBottomMargin(0);
+    });
+
+    // Or, if you have a close button:
+    const closeBtn = banner.querySelector("[data-vf-js-banner-close]");
+    if (closeBtn) {
+      closeBtn.addEventListener("click", () => {
+        updateChatbotBottomMargin(0);
+      });
+    }
     return banner.offsetHeight || 0;
   }
   return 0;
