e215a57297e58f2ca141049333202ecd597b9bb9
Bhushan Palsapure <bhushanp@ebi.ac.uk>
2026-05-21 09:13:44 +0100

    Component release 20260521-01

--- tools/vf-component-library/src/site/_data/siteConfig.js
+++ tools/vf-component-library/src/site/_data/siteConfig.js
@@ -15,7 +15,7 @@ module.exports = {
   // vfVersion: "develop",
   // vfVersionPrefix: "dev.",
   // use this when tagging a release
-  vfVersion: "v2.5.27",
+  vfVersion: "v2.5.28",
   vfVersionPrefix: "",
   sections: {
     about: {
